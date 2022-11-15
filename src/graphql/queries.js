4b571face6bf8150a4c68a121e685adca4b7185c
Brian <bstanton773@gmail.com>
2022-11-15 12:29:40 -0600

    Add get single user by id graphql query

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -1,5 +1,5 @@
 // Import Types from GraphQL
-const { GraphQLList } = require('graphql');
+const { GraphQLList, GraphQLID } = require('graphql');
 // Import our own created type
 const { UserType } = require('./types');
 // Import model so we can get data from MongoDB
@@ -8,13 +8,25 @@ const { User } = require('../models');
 
 const users = {
     type: new GraphQLList(UserType),
-    description: 'We are going to get every single user from the database and display it here',
-    resolve(parents, args){
+    description: 'Query all users from database',
+    resolve(parent, args){
         return User.find()
+    }
+}
+
+const user = {
+    type: UserType,
+    desciption: 'Query single user from database by id',
+    args: {
+        id: { type: GraphQLID }
+    },
+    resolve(parent, args){
+        return User.findById(args.id)
     }
 }
 
 
 module.exports = {
-    users
+    users,
+    user
 }
