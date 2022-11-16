88f65a84a535f12e9aaf1ee86c177a6031acc614
Brian <bstanton773@gmail.com>
2022-11-16 15:40:33 -0600

    Add POST create quiz that will add new quiz with questions to db

--- src/routes/quiz/index.js
+++ src/routes/quiz/index.js
@@ -3,6 +3,7 @@ const QuizDashboardRouter = require('express').Router();
 
 QuizDashboardRouter.route('/create')
     .get(require('./editor'))
+    .post(require('./create'))
 
 
 module.exports = QuizDashboardRouter;
