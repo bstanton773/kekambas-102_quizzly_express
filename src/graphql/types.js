10057ae0487d1fb25b8be92b754de7a4234e3fba
Brian <bstanton773@gmail.com>
2022-11-17 12:22:27 -0600

    Update UserType and QuizType to add submissions and avgScore

--- src/graphql/types.js
+++ src/graphql/types.js
@@ -1,7 +1,7 @@
 // Import built-in graphql type
-const { GraphQLObjectType, GraphQLInputObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLList } = require('graphql');
+const { GraphQLObjectType, GraphQLInputObjectType, GraphQLID, GraphQLString, GraphQLInt, GraphQLList, GraphQLFloat } = require('graphql');
 // Import User, Quiz model to query
-const { User, Quiz, Question } = require('../models')
+const { User, Quiz, Question, Submission } = require('../models')
 
 
 const UserType = new GraphQLObjectType(
@@ -16,6 +16,12 @@ const UserType = new GraphQLObjectType(
                 type: GraphQLList(QuizType),
                 resolve(parent, args){
                     return Quiz.find({ userId: parent.id })
+                }
+            },
+            submissions: {
+                type: GraphQLList(SubmissionType),
+                resolve(parent, args){
+                    return Submission.find({ userId: parent.id })
                 }
             }
         })
@@ -44,6 +50,25 @@ const QuizType = new GraphQLObjectType(
                 type: GraphQLList(QuestionType),
                 resolve(parent, args){
                     return Question.find({ quizId: parent.id })
+                }
+            },
+            submissions: {
+                type: GraphQLList(SubmissionType),
+                resolve(parent, args){
+                    return Submission.find({ quizId: parent.id })
+                }
+            },
+            avgScore: {
+                type: GraphQLFloat,
+                async resolve(parent, args){
+                    const submissions = await Submission.find( {quizId: parent.id} );
+                    let score = 0;
+
+                    for (const submission of submissions){
+                        score += submission.score
+                    };
+
+                    return score / submissions.length;
                 }
             }
         })
